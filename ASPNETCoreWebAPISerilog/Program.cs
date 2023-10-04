efd359da588f096d70ea7b05153c56745dbf1929
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2023-10-04 17:05:07 -0500

    install serilog.aspdotnetcore nuget first log into file

--- ASPNETCoreWebAPISerilog/Program.cs
+++ ASPNETCoreWebAPISerilog/Program.cs
@@ -1,3 +1,5 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .WriteTo.Console()
+    .WriteTo.File("logs/myfulLog-.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
 
 var app = builder.Build();
 
